3f88275dedbbac658767bcf3dd477e73ad1ffab4
batorrakovics <rakovics.bator@gmail.com>
2017-07-11 10:00:58 +0200

    add meg bonus lol

--- gittie.py
+++ gittie.py
@@ -1,2 +1,8 @@
 def introducion():
     print("F you Gittie")
+
+def add():
+    print("Add Git to the fucking rubbish bin")
+
+def bonus():
+    print("Here's a joke, what's worse than cancer? - Git.")
